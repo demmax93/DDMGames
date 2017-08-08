b591ce4f50dcae4040e43d9cb963ed2fa521e6fb
made0915 <maksim.demchenko@NetCracker.com>
2017-08-08 16:30:50 +0400

    first commit - hello word for LGDX

---
+++ desktop/src/com/ddm/test/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ddm.test.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ddm.test.game.MyGameTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGameTest(), config);
+	}
+}
---
+++ html/src/com/ddm/test/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ddm.test.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ddm.test.game.MyGameTest;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGameTest();
+        }
+}

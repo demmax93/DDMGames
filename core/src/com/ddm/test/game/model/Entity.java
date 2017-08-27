3ea31c798c2b4bf178a062da1c09662a4affad44
Karp <dimankarpenko@yandex.ru>
2017-08-27 19:45:14 +0400

    Fix player position
    Deleted old files

--- core/src/com/ddm/test/game/model/Entity.java
+++ core/src/com/ddm/test/game/model/Entity.java
@@ -11,18 +11,16 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.ddm.test.game.MapManager;
 import com.ddm.test.game.Utility;
+import com.ddm.test.game.view.screens.MainGameScreen;
 
 import java.util.UUID;
 
-/**
- * DMKA
- * 27.08.2017
- */
 public class Entity {
     private static final String TAG = Entity.class.getSimpleName();
-    private static final String defaultSpritePath ="core/assets/texture/hero_sprites.png";
+    private static final String defaultSpritePath = "core/assets/texture/hero_sprites.png";
     private float planeSpeed, velocity = 10.0f;
-    //private Vector2 velocity;
+    private float tempPositionX;
+    private float tempPositionY;
     private String entityID;
     private Direction currentDirection = Direction.UP;
     private Direction previousDirection = Direction.UP;
@@ -49,160 +47,180 @@ public class Entity {
         UP;
     }
 
-    public Entity(){
+    public Entity() {
         initEntity();
     }
 
-    public void initEntity(){
+    public void initEntity() {
         this.entityID = UUID.randomUUID().toString();
         this.nextPlayerPosition = new Vector2();
         this.currentPlayerPosition = new Vector2();
         this.boundingBox = new Rectangle();
-        //this.velocity = new Vector2(2f,2f);
         Utility.loadTextureAsset(defaultSpritePath);
         loadDefaultSprite();
         loadAllAnimations();
     }
 
-    public void update(float delta){
-        frameTime = (frameTime + delta)%5; //Want to avoid overflow
-        //We want the hitbox to be at the feet for a better feel
+    public void update(float delta) {
+        frameTime = (frameTime + delta) % 5;
         setBoundingBoxSize(0f, 0.5f);
     }
 
-    public void init(float startX, float startY){
+    public void init(float startX, float startY) {
         this.currentPlayerPosition.x = startX;
         this.currentPlayerPosition.y = startY;
         this.nextPlayerPosition.x = startX;
         this.nextPlayerPosition.y = startY;
     }
 
-    public void setBoundingBoxSize(float percentageWidthReduced, float percentageHeightReduced){
-        //Update the current bounding box
+    public void setBoundingBoxSize(float percentageWidthReduced, float percentageHeightReduced) {
         float width;
         float height;
         float widthReductionAmount = 1.0f - percentageWidthReduced;
-        //.8f for 20% (1 - .20)
         float heightReductionAmount = 1.0f - percentageHeightReduced;
-        //.8f for 20% (1 - .20)
-        if( widthReductionAmount > 0 && widthReductionAmount < 1){
+        if (widthReductionAmount > 0 && widthReductionAmount < 1) {
             width = FRAME_WIDTH * widthReductionAmount;
-        } else{
+        } else {
             width = FRAME_WIDTH;
         }
 
-        if( heightReductionAmount > 0 && heightReductionAmount < 1){
+        if (heightReductionAmount > 0 && heightReductionAmount < 1) {
             height = FRAME_HEIGHT * heightReductionAmount;
-        } else{
+        } else {
             height = FRAME_HEIGHT;
         }
 
-        if( width == 0 || height == 0){
-            Gdx.app.debug(TAG, "Width and Height are 0!! " + width +":" + height);
+        if (width == 0 || height == 0) {
+            Gdx.app.debug(TAG, "Width and Height are 0!! " + width + ":" + height);
         }
 
-        //Need to account for the unitscale, since the map coordinates will be in pixels
         float minX;
         float minY;
-        if( MapManager.UNIT_SCALE > 0 ) {
+        if (MapManager.UNIT_SCALE > 0) {
             minX = nextPlayerPosition.x / MapManager.UNIT_SCALE;
             minY = nextPlayerPosition.y / MapManager.UNIT_SCALE;
-        } else{
+        } else {
             minX = nextPlayerPosition.x;
             minY = nextPlayerPosition.y;
         }
         boundingBox.set(minX, minY, width, height);
     }
 
-    private void loadDefaultSprite(){
+    private void loadDefaultSprite() {
         Texture texture = Utility.getTextureAsset(defaultSpritePath);
         TextureRegion[][] textureFrames = TextureRegion.split(texture, FRAME_WIDTH, FRAME_HEIGHT);
-        frameSprite = new Sprite(textureFrames[0][0].getTexture(), 0,0,FRAME_WIDTH, FRAME_HEIGHT);
+        frameSprite = new Sprite(textureFrames[0][0].getTexture(), 0, 0, FRAME_WIDTH, FRAME_HEIGHT);
         currentFrame = textureFrames[0][0];
     }
-    private void loadAllAnimations(){
+
+    private void loadAllAnimations() {
         //Walking animation
         Texture texture = Utility.getTextureAsset(defaultSpritePath);
         TextureRegion[][] textureFrames = TextureRegion.split(texture, FRAME_WIDTH, FRAME_HEIGHT);
         walkUpFrames = new Array<TextureRegion>(39);
-        for (int i=0, j = 0; j < 39; j++) {
-           TextureRegion region = textureFrames[j][i];
-           if( region == null ){
-              Gdx.app.debug(TAG, "Got null animation frame " + i + "," + j);
-           }
-           walkUpFrames.insert(j, region);
+        for (int i = 0, j = 0; j < 39; j++) {
+            TextureRegion region = textureFrames[j][i];
+            if (region == null) {
+                Gdx.app.debug(TAG, "Got null animation frame " + i + "," + j);
+            }
+            walkUpFrames.insert(j, region);
         }
-        walkUpAnimation = new Animation(0.08f, walkUpFrames,Animation.PlayMode.LOOP);
+        walkUpAnimation = new Animation(0.08f, walkUpFrames, Animation.PlayMode.LOOP);
     }
 
-    public void dispose(){
+    public void dispose() {
         Utility.unloadAsset(defaultSpritePath);
     }
 
-    public void setState(State state){
+    public void setState(State state) {
         this.state = state;
     }
 
-    public Sprite getFrameSprite(){
+    public Sprite getFrameSprite() {
         return frameSprite;
     }
 
-    public TextureRegion getFrame(){
+    public TextureRegion getFrame() {
         return currentFrame;
     }
 
-    public Vector2 getCurrentPosition(){
+    public Vector2 getCurrentPosition() {
         return currentPlayerPosition;
     }
 
-    public void setCurrentPosition(float currentPositionX, float currentPositionY){
+    public void setCurrentPosition(float currentPositionX, float currentPositionY) {
         frameSprite.setX(currentPositionX);
         frameSprite.setY(currentPositionY);
         this.currentPlayerPosition.x = currentPositionX;
         this.currentPlayerPosition.y = currentPositionY;
     }
 
-    public void setDirection(Direction direction,  float deltaTime){
+    public void setDirection(Direction direction, float deltaTime) {
         this.previousDirection = this.currentDirection;
         this.currentDirection = direction;
         //Look into the appropriate variable when changing position
         switch (currentDirection) {
-            case UP :
+            case UP:
                 currentFrame = (TextureRegion) walkUpAnimation.getKeyFrame(frameTime);
                 break;
             default:
                 break;
         }
     }
-    public void setNextPositionToCurrent(){
+
+    public void setNextPositionToCurrent() {
         setCurrentPosition(nextPlayerPosition.x, nextPlayerPosition.y);
     }
 
-    public void calculateNextPosition(Direction currentDirection, float deltaTime){
+    public void calculateNextPosition(Direction currentDirection, float deltaTime) {
         if (currentDirection.equals(Direction.UP)) {
             planeSpeed += velocity * deltaTime;
-        }else if (currentDirection.equals(Direction.DOWN)) {
+        } else if (currentDirection.equals(Direction.DOWN)) {
             planeSpeed -= velocity * deltaTime;
-        }else downSpeed(deltaTime);
+        } else downSpeed(deltaTime);
 
         planeSpeed = sliceSpeed();
 
         float rotationSpeed = 30f;
-        if (currentDirection.equals(Direction.LEFT)){
+        if (currentDirection.equals(Direction.LEFT)) {
             frameSprite.rotate(rotationSpeed * deltaTime * planeSpeed);
-        }else if (currentDirection.equals(Direction.RIGHT)){
+        } else if (currentDirection.equals(Direction.RIGHT)) {
             frameSprite.rotate(-rotationSpeed * deltaTime * planeSpeed);
         }
+        tempPositionX = frameSprite.getX() + MathUtils.cosDeg(frameSprite.getRotation() + 90) * planeSpeed * deltaTime;
+        tempPositionY = frameSprite.getY() + MathUtils.sinDeg(frameSprite.getRotation() + 90) * planeSpeed * deltaTime;
+        collisionsWithBackground();
 
-        frameSprite.setPosition(frameSprite.getX() + MathUtils.cosDeg(frameSprite.getRotation() + 90) * planeSpeed * deltaTime,
-                frameSprite.getY() + MathUtils.sinDeg(frameSprite.getRotation() + 90) * planeSpeed * deltaTime);
+        frameSprite.setPosition(tempPositionX, tempPositionY);
 
-        nextPlayerPosition.x =frameSprite.getX();
-        nextPlayerPosition.y =frameSprite.getY();
+        nextPlayerPosition.x = frameSprite.getX();
+        nextPlayerPosition.y = frameSprite.getY();
 
     }
+
+    private void collisionsWithBackground() {
+        float x = tempPositionX;
+        float y = tempPositionY;
+
+        if (x < frameSprite.getWidth() / 100 - 1.2f) {
+            x = frameSprite.getWidth() / 100 - 1.2f;
+        }
+        if (y < frameSprite.getHeight() / 200 - 1) {
+            y = frameSprite.getHeight() / 200 - 1;
+        }
+        if (x > MainGameScreen.getLayerMap().getTileWidth() * MainGameScreen.getLayerMap().getWidth() / 100 - 0.8f) {
+            x = MainGameScreen.getLayerMap().getTileWidth() * MainGameScreen.getLayerMap().getWidth() / 100 - 0.8f;
+        }
+        if (y > MainGameScreen.getLayerMap().getTileHeight() * MainGameScreen.getLayerMap().getHeight() / 100 - 1f) {
+            y = MainGameScreen.getLayerMap().getTileHeight() * MainGameScreen.getLayerMap().getHeight() / 100 - 1f;
+        }
+        tempPositionX = x;
+        tempPositionY = y;
+
+    }
+
     private void downSpeed(float delta) {
-        if(planeSpeed > velocity * delta) {
+        if (planeSpeed > velocity * delta) {
             planeSpeed -= velocity * delta;
         } else if (planeSpeed < -velocity * delta) {
             planeSpeed += velocity * delta;
@@ -211,10 +229,10 @@ public class Entity {
 
     private float sliceSpeed() {
         float maxSpeed = 5.0f;
-        if(maxSpeed < planeSpeed) {
+        if (maxSpeed < planeSpeed) {
             return maxSpeed;
         }
-        if(-maxSpeed > planeSpeed) {
+        if (-maxSpeed > planeSpeed) {
             return -maxSpeed;
         }
         return planeSpeed;
--- core/src/com/ddm/test/game/view/screens/MainGameScreen.java
+++ core/src/com/ddm/test/game/view/screens/MainGameScreen.java
@@ -16,10 +16,6 @@ import com.ddm.test.game.MapManager;
 import com.ddm.test.game.control.PlayerController;
 import com.ddm.test.game.model.Entity;
 
-/**
- * DMKA
- * 25.08.2017
- */
 public class MainGameScreen implements Screen {
     private static final String TAG =
             MainGameScreen.class.getSimpleName();
@@ -40,8 +36,9 @@ public class MainGameScreen implements Screen {
     private OrthogonalTiledMapRenderer mapRenderer = null;
     private OrthographicCamera camera = null;
     private static MapManager mapMgr;
+    private static TiledMapTileLayer layerMap = null;
     public static final double TEEWS = 0.04; // opozdanie camery
-    TiledMapTileLayer layerMap ;
+
 
     public MainGameScreen() {
         mapMgr = new MapManager();
@@ -56,7 +53,7 @@ public class MainGameScreen implements Screen {
         //get the current size
         camera = new OrthographicCamera();
         camera.setToOrtho(false, VIEWPORT.viewportWidth, VIEWPORT.viewportHeight);
-        mapRenderer = new OrthogonalTiledMapRenderer (mapMgr.getCurrentMap(), MapManager.UNIT_SCALE);
+        mapRenderer = new OrthogonalTiledMapRenderer(mapMgr.getCurrentMap(), MapManager.UNIT_SCALE);
         mapRenderer.setView(camera);
         Gdx.app.debug(TAG, "UnitScale value is:" + mapRenderer.getUnitScale());
         player = new Entity();
@@ -64,7 +61,7 @@ public class MainGameScreen implements Screen {
         currentPlayerSprite = player.getFrameSprite();
         controller = new PlayerController(player);
         Gdx.input.setInputProcessor(controller);
-        layerMap = (TiledMapTileLayer)mapMgr.getCurrentMap().getLayers().get(0);
+        layerMap = (TiledMapTileLayer) mapMgr.getCurrentMap().getLayers().get(0);
     }
 
     @Override
@@ -79,33 +76,33 @@ public class MainGameScreen implements Screen {
         player.update(delta);
         currentPlayerFrame = player.getFrame();
         updatePortalLayerActivation(player.boundingBox);
-        if( !isCollisionWithMapLayer(player.boundingBox) ){
+        if (!isCollisionWithMapLayer(player.boundingBox)) {
             player.setNextPositionToCurrent();
         }
         controller.update(delta);
         mapRenderer.setView(camera);
         mapRenderer.render();
         mapRenderer.getBatch().begin();
-        mapRenderer.getBatch().draw(currentPlayerFrame, currentPlayerSprite.getX(), currentPlayerSprite.getY(), 1,1);
+        mapRenderer.getBatch().draw(currentPlayerFrame, currentPlayerSprite.getX(), currentPlayerSprite.getY(), 1, 1);
         mapRenderer.getBatch().end();
     }
 
-    private void setCameraPosition(){
-        float x = (float) (camera.position.x +((currentPlayerSprite.getX() - camera.position.x) * TEEWS));
-        float y = (float) (camera.position.y +((currentPlayerSprite.getY() - camera.position.y) * TEEWS));
-        if(x < camera.viewportWidth/2) {
-            x = camera.viewportWidth/2;
+    private void setCameraPosition() {
+        float x = (float) (camera.position.x + ((currentPlayerSprite.getX() - camera.position.x) * TEEWS));
+        float y = (float) (camera.position.y + ((currentPlayerSprite.getY() - camera.position.y) * TEEWS));
+        if (x < camera.viewportWidth / 2) {
+            x = camera.viewportWidth / 2;
         }
-        if(y < camera.viewportHeight/2) {
-            y = camera.viewportHeight/2;
+        if (y < camera.viewportHeight / 2) {
+            y = camera.viewportHeight / 2;
         }
-        if(x > layerMap.getTileWidth() * layerMap.getWidth()/100 - camera.viewportWidth/2) {
-            x = layerMap.getTileWidth() * layerMap.getWidth()/100 - camera.viewportWidth/2;
+        if (x > layerMap.getTileWidth() * layerMap.getWidth() / 100 - camera.viewportWidth / 2) {
+            x = layerMap.getTileWidth() * layerMap.getWidth() / 100 - camera.viewportWidth / 2;
         }
-        if(y > layerMap.getTileHeight() * layerMap.getHeight()/100 - camera.viewportHeight/2) {
-            y = layerMap.getTileHeight() * layerMap.getHeight()/100 - camera.viewportHeight/2;
+        if (y > layerMap.getTileHeight() * layerMap.getHeight() / 100 - camera.viewportHeight / 2) {
+            y = layerMap.getTileHeight() * layerMap.getHeight() / 100 - camera.viewportHeight / 2;
         }
-        camera.position.set(x,y, 0);
+        camera.position.set(x, y, 0);
         camera.update();
     }
 
@@ -129,7 +126,7 @@ public class MainGameScreen implements Screen {
         mapRenderer.dispose();
     }
 
-    private void setupViewport(int width, int height){
+    private void setupViewport(int width, int height) {
         //Make the viewport a percentage of the total display area
         VIEWPORT.virtualWidth = width;
         VIEWPORT.virtualHeight = height;
@@ -142,30 +139,30 @@ public class MainGameScreen implements Screen {
         //aspect ratio for current viewport
         VIEWPORT.aspectRatio = (VIEWPORT.virtualWidth / VIEWPORT.virtualHeight);
         //update viewport if there could be skewing
-        if( VIEWPORT.physicalWidth / VIEWPORT.physicalHeight >= VIEWPORT.aspectRatio){
+        if (VIEWPORT.physicalWidth / VIEWPORT.physicalHeight >= VIEWPORT.aspectRatio) {
             //Letterbox left and right
-            VIEWPORT.viewportWidth = VIEWPORT.viewportHeight * (VIEWPORT.physicalWidth/VIEWPORT.physicalHeight);
+            VIEWPORT.viewportWidth = VIEWPORT.viewportHeight * (VIEWPORT.physicalWidth / VIEWPORT.physicalHeight);
             VIEWPORT.viewportHeight = VIEWPORT.virtualHeight;
-        }else{
+        } else {
             //letterbox above and below
             VIEWPORT.viewportWidth = VIEWPORT.virtualWidth;
-            VIEWPORT.viewportHeight = VIEWPORT.viewportWidth * (VIEWPORT.physicalHeight/VIEWPORT.physicalWidth);
+            VIEWPORT.viewportHeight = VIEWPORT.viewportWidth * (VIEWPORT.physicalHeight / VIEWPORT.physicalWidth);
         }
-        Gdx.app.debug(TAG, "WorldRenderer: virtual: (" + VIEWPORT.virtualWidth + "," + VIEWPORT.virtualHeight + ")" );
+        Gdx.app.debug(TAG, "WorldRenderer: virtual: (" + VIEWPORT.virtualWidth + "," + VIEWPORT.virtualHeight + ")");
         Gdx.app.debug(TAG, "WorldRenderer: viewport: (" + VIEWPORT.viewportWidth + "," + VIEWPORT.viewportHeight + ")");
         Gdx.app.debug(TAG, "WorldRenderer: physical: (" + VIEWPORT.physicalWidth + "," + VIEWPORT.physicalHeight + ")");
     }
 
-    private boolean isCollisionWithMapLayer(Rectangle boundingBox){
-        MapLayer mapCollisionLayer =  mapMgr.getCollisionLayer();
-        if( mapCollisionLayer == null ){
+    private boolean isCollisionWithMapLayer(Rectangle boundingBox) {
+        MapLayer mapCollisionLayer = mapMgr.getCollisionLayer();
+        if (mapCollisionLayer == null) {
             return false;
         }
         Rectangle rectangle = null;
-        for( MapObject object: mapCollisionLayer.getObjects()){
-            if(object instanceof RectangleMapObject) {
-                rectangle = ((RectangleMapObject)object).getRectangle();
-                if( boundingBox.overlaps(rectangle) ){
+        for (MapObject object : mapCollisionLayer.getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                rectangle = ((RectangleMapObject) object).getRectangle();
+                if (boundingBox.overlaps(rectangle)) {
                     return true;
                 }
             }
@@ -173,21 +170,21 @@ public class MainGameScreen implements Screen {
         return false;
     }
 
-    private boolean updatePortalLayerActivation(Rectangle boundingBox){
-        MapLayer mapPortalLayer =  mapMgr.getPortalLayer();
-        if( mapPortalLayer == null ){
+    private boolean updatePortalLayerActivation(Rectangle boundingBox) {
+        MapLayer mapPortalLayer = mapMgr.getPortalLayer();
+        if (mapPortalLayer == null) {
             return false;
         }
         Rectangle rectangle = null;
-        for( MapObject object: mapPortalLayer.getObjects()){
-            if(object instanceof RectangleMapObject) {
-                rectangle = ((RectangleMapObject)object).getRectangle();
-                if( boundingBox.overlaps(rectangle) ){
+        for (MapObject object : mapPortalLayer.getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                rectangle = ((RectangleMapObject) object).getRectangle();
+                if (boundingBox.overlaps(rectangle)) {
                     String mapName = object.getName();
-                    if( mapName == null ) {
+                    if (mapName == null) {
                         return false;
                     }
-                    mapMgr.setClosestStartPositionFromScaledUnits (player.getCurrentPosition());
+                    mapMgr.setClosestStartPositionFromScaledUnits(player.getCurrentPosition());
                     mapMgr.loadMap(mapName);
                     player.init(mapMgr.getPlayerStartUnitScaled().x, mapMgr.getPlayerStartUnitScaled().y);
                     mapRenderer.setMap(mapMgr.getCurrentMap());
@@ -197,5 +194,9 @@ public class MainGameScreen implements Screen {
             }
         }
         return false;
+    }
+
+    public static TiledMapTileLayer getLayerMap() {
+        return layerMap;
     }
 }
